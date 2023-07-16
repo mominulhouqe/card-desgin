dbc93e89266cb7e5084e9819ff30e6c1501140d3
mominul <ulhouqemomin39@gmail.com>
2023-07-16 12:06:45 +0600

    updated calender

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -8,22 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
   calendar: number[][] = [];
   currentDate!: Date;
+  selectedMonth!: number;
+  selectedYear!: number;
 
   constructor() { }
 
   ngOnInit(): void {
     this.currentDate = new Date();
+    this.selectedMonth = this.currentDate.getMonth();
+    this.selectedYear = this.currentDate.getFullYear();
     this.generateCalendar();
   }
 
   generateCalendar(): void {
-    const year = this.currentDate.getFullYear();
-    const month = this.currentDate.getMonth();
-
-    const firstDay = new Date(year, month, 1).getDay();
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDay = new Date(this.selectedYear, this.selectedMonth, 1).getDay();
+    const daysInMonth = new Date(this.selectedYear, this.selectedMonth + 1, 0).getDate();
 
     let day = 1;
+    this.calendar = [];
+
     for (let i = 0; i < 6; i++) {
       const week: number[] = [];
       for (let j = 0; j < 7; j++) {
@@ -46,7 +49,11 @@ export class CalendarComponent implements OnInit {
     const currentMonth = currentDate.getMonth();
     const currentYear = currentDate.getFullYear();
 
-    return day === currentDay && this.currentDate.getMonth() === currentMonth && this.currentDate.getFullYear() === currentYear;
+    return (
+      day === currentDay &&
+      this.selectedYear === currentYear &&
+      this.selectedMonth === currentMonth
+    );
   }
 
   isToday(day: number): boolean {
@@ -55,6 +62,28 @@ export class CalendarComponent implements OnInit {
     const currentMonth = currentDate.getMonth();
     const currentYear = currentDate.getFullYear();
 
-    return day === currentDay && this.currentDate.getMonth() === currentMonth && this.currentDate.getFullYear() === currentYear;
+    return (
+      day === currentDay &&
+      this.selectedYear === currentYear &&
+      this.selectedMonth === currentMonth
+    );
+  }
+
+  navigateToMonth(month: number): void {
+    if (month < 0) {
+      this.selectedYear--;
+      this.selectedMonth = 11; // Set to December (11) if going back from January
+    } else if (month > 11) {
+      this.selectedYear++;
+      this.selectedMonth = 0; // Set to January (0) if going forward from December
+    } else {
+      this.selectedMonth = month;
+    }
+    this.generateCalendar();
+  }
+
+  navigateToYear(year: number): void {
+    this.selectedYear = year;
+    this.generateCalendar();
   }
 }
